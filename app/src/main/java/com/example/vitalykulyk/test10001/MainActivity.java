3cf6a8b449512e6095e439c0b47d7414fd17a1fa
Vitalii Kulyk <vitcool@ukr.net>
2016-03-13 16:48:50 +0200

    init commit

---
+++ app/src/main/java/com/example/vitalykulyk/test10001/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vitalykulyk.test10001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                finish();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
